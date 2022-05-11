fd5d1e143c719c1e0842382bab64c0d01d715d8d
oaleekis <oalexsanderm@gmail.com>
2022-05-11 10:34:49 -0300

    confirmar

--- script.js
+++ script.js
@@ -6,41 +6,41 @@ let txtEles = window.document.getElementById("txtEles")
 
 
 
-function addPontos(type){
-    if(type == 1 && pontosNos >= 0 && pontosNos < 13){
+function addPontos(type) {
+    if (type == 1 && pontosNos >= 0 && pontosNos < 13) {
         pontosNos++
-        
-        
-    }else if(type == 2 && pontosEles >= 0 && pontosEles < 13){
+
+
+    } else if (type == 2 && pontosEles >= 0 && pontosEles < 13) {
         pontosEles++
-        
-    }else{
+
+    } else {
         console.log('Pontos invalidos')
     }
 
-txtEles.innerHTML = pontosEles
-txtNos.innerHTML = pontosNos
-   
+    txtEles.innerHTML = pontosEles
+    txtNos.innerHTML = pontosNos
+
 }
 
-function removePontos(type){
-    if(type == 1 && pontosNos > 0 && pontosNos <= 13){
+function removePontos(type) {
+    if (type == 1 && pontosNos > 0 && pontosNos <= 13) {
         pontosNos--
-        
-        
-    }else if(type == 2 && pontosEles > 0 && pontosEles <= 13){
+
+
+    } else if (type == 2 && pontosEles > 0 && pontosEles <= 13) {
         pontosEles--
-        
-    }else{
+
+    } else {
         console.log('Pontos invalidos')
     }
 
-txtEles.innerHTML = pontosEles
-txtNos.innerHTML = pontosNos
-   
+    txtEles.innerHTML = pontosEles
+    txtNos.innerHTML = pontosNos
+
 }
 
-function zerar(){
+function zerar() {
     pontosEles = 0
     pontosNos = 0
     txtEles.innerHTML = pontosEles
